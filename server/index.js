b6209c54718a34dfcda3f51754376e06f70ff7d4
SumitSharma <sumitkumarsharma2748@gmail.com>
2026-03-27 03:05:58 +0530

    fix:update cors error

--- server/index.js
+++ server/index.js
@@ -15,7 +15,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
