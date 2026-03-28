d93c1d12e80746c9a36cb6ba25c3d00f02a6af4f
SumitSharma <sumitkumarsharma2748@gmail.com>
2026-03-28 13:11:43 +0530

    Fix: Switched Nodemailer

--- server/utils/mail.utils.js
+++ server/utils/mail.utils.js
@@ -5,19 +5,12 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.USER_EMAIL,
     pass: process.env.USER_PASSWORD,
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
-  pool: true,
-  maxConnections: 1,
-  maxMessages: Infinity,
 });
 
 export const sendBookingEmail = async (userMail, userName, eventTitle) => {
