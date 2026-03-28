b4d5d5e6a6779b6f692d1f7553ca215d12de088b
SumitSharma <sumitkumarsharma2748@gmail.com>
2026-03-28 12:57:24 +0530

    Fix: Nodemailer IPv6 crash on Render

--- server/utils/mail.utils.js
+++ server/utils/mail.utils.js
@@ -4,10 +4,15 @@ import nodemailer from "nodemailer";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.USER_EMAIL,
     pass: process.env.USER_PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
   pool: true,
   maxConnections: 1,
